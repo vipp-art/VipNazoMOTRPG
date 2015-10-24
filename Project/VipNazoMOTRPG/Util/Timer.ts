8cb002c627c600fbebaddf31de40d079dd4a1db6
nazo <nazo>
2015-10-25 01:05:05 +0900

    タイマー追加。

---
+++ Project/VipNazoMOTRPG/Util/Timer.ts
@@ -0,0 +1,68 @@
+﻿module util {
+    /** 一定時間後に呼ばれるタイマー */
+    export class Timer {
+        /** イベント発火時のコールバック */
+        private callback_: Function;
+        /** コールバックの所有者 */
+        private owner_: Object;
+        /** イベント感覚 */
+        private interval_: number = 100;
+
+        /** タグ */
+        private tag_: string = '';
+
+        /**
+         * タイマーの生成
+         * @param tag タグ
+         * @param callback イベント発火時に呼ばれる関数 プロトタイプはfunction(eventTarget:Timer)
+         * @param owner callbackの親となるオブジェクト
+         */
+        static createTimer(tag: string, callback: Function, owner?: Object): Timer {
+            var result: Timer = new __internal.HTMLTimer();
+            result.tag_ = tag;
+            result.callback_ = callback;
+            result.owner_ = owner;
+            return result;
+        }
+
+        /** */
+        get interval(): number {
+            return this.interval_;
+        }
+
+        /** */
+        get callback(): Function {
+            return this.callback_;
+        }
+
+        /** */
+        get owner(): Object {
+            return this.owner_;
+        }
+
+        /** 期間の設定 */
+        setInterval(milliseconds: number): void {
+            this.interval_ = milliseconds;
+        }
+
+        /** タグの指定 */
+        set tag(tag: string) {
+            this.tag_ = tag;
+        }
+
+        /** タグの取得 */
+        get tag(): string {
+            return this.tag_;
+        }
+
+        /** タイマーの開始 */
+        start(): void {
+            throw '未実装';
+        }
+
+        /** 停止 */
+        stop(): void {
+            throw '未実装';
+        }
+    }
+}
---
+++ Project/VipNazoMOTRPG/Util/impl/Timer.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../Timer.ts" />
+module util.__internal {
+    export class HTMLTimer extends util.Timer {
+        private id_: number = -1;
+
+        /** イベント発火 */
+        private tick(): void {
+            this.callback.call(this.owner, this);
+        }
+
+        /** タイマーの開始 */
+        start(): void {
+            this.id_= setTimeout(() => { this.tick(); }, this.interval);
+        }
+
+        /** 停止 */
+        stop(): void {
+            if (this.id_ >= 0) {
+                clearTimeout(this.id_);
+            }
+            this.id_ = -1;
+        }
+
+    }
+}
