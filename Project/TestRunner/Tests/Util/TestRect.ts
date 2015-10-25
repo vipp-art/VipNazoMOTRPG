7c6cde63952f1b20125b0a59034b2331a2de6620
nazo <nazo>
2015-10-25 21:54:14 +0900

    jasmineのTypeScript定義ファイル追加。 Rectのテストコード追加。

---
+++ Project/TestRunner/Tests/Util/TestRect.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../../VipNazoMOTRPG/Util/Point.ts" />
+/// <reference path="../../../VipNazoMOTRPG/Util/Rect.ts" />
+/// <reference path="../../Scripts/typings/jasmine/jasmine.d.ts" />
+describe('util.Rect', () => {
+    var rect: util.Rect;
+
+    beforeEach(() => {
+        rect = new util.Rect(1, 2, 10, 10);
+    });
+
+    it('center', () => {
+        var center:util.Point = rect.center;
+        expect(center.x).toBe(1 + 10 * .5);
+        expect(center.y).toBe(2 + 10 * .5);
+    });
+
+    it('isCross', () => {
+        expect(rect.isCross(2, 3)).toBe(true);
+        expect(rect.isCross(12, 3)).toBe(false);
+        expect(rect.isCross(2, 13)).toBe(false);
+    });
+} );
